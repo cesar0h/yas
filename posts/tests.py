fb7a2763e0e3ccedc2d88ccc4e3b1b44687f950e
César  Ortiz Hernández <cesar_0h@Cesars-MacBook-Air-2.local>
2022-08-29 22:54:53 -0500

    initial commit

--- posts/tests.py
+++ posts/tests.py
@@ -5,19 +5,11 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 
 # Create your tests here.
-class BoardpageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response=self.client.get("/board/")
-        self.assertEqual(response.status_code,200)
+from .models import Post
 
-    def test_url_available_by_name(self):
-        response=self.client.get(reverse("board"))
-        self.assertEqual(response.status_code,200)
-
-    def test_template_name_correct(self):
-        response=self.client.get(reverse("board"))
-        self.assertTemplateUsed(response,"board.html")
-
-    def test_template_content(self):
-        response=self.client.get(reverse("board"))
-        self.assertContains(response,"<h1>Board page</h1>")
+class PostTests(TestCase): 
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This is a test!")
+    def test_model_content(self): 
+        self.assertEqual(self.post.text, "This is a test!")
