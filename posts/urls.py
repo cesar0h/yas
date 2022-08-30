915af2b009339800c636986a35fa0dbea4a4a837
César  Ortiz Hernández <cesar_0h@Cesars-MacBook-Air-2.local>
2022-08-29 22:30:30 -0500

    initial commit

--- posts/urls.py
+++ posts/urls.py
@@ -1,3 +1,3 @@
 from django.urls import path 
 from posts.views import BoardView
-urlpatterns = [path("", BoardView.as_view(), name="board"),]
+urlpatterns = [path("", BoardView.as_view(), name="posts"),]
